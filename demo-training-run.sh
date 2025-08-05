8d8650f1951210a92e0a9e39a28110d3a3c5a4d2
Gavin Ji <71982068+Lyttr@users.noreply.github.com>
2025-08-05 13:27:10 -0700

    Update demo-training-run.sh

--- demo-training-run.sh
+++ demo-training-run.sh
@@ -41,7 +41,7 @@ EPOCH_SAVE=1 # save every 10 "miniepochs" (1 miniepoch = 40320 * ctx_len tokens)
 N_NODE=1 # number of nodes
 GPU_PER_NODE=1 # number of GPUs per node
 #
-DS_BUCKET_MB=10 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affects speed & vram usage)
+DS_BUCKET_MB=2 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affects speed & vram usage)
 #
 python train.py --load_model "0" --wandb "rna-rwkv" --proj_dir $PROJ_DIR --my_testing $MODEL_TYPE \
  --ctx_len $CTX_LEN --my_pile_stage 3 --epoch_count 999999 --epoch_begin 0 \
