82004554298f4faf2ba9a34be10d374a521650bb
Gavin Ji <71982068+Lyttr@users.noreply.github.com>
2025-08-15 06:40:29 -0700

    Update demo-training-run.sh

--- demo-training-run.sh
+++ demo-training-run.sh
@@ -13,11 +13,11 @@
 MODEL_TYPE="x052" # x060 => rwkv-6.0
 # MODEL_TYPE="mamba" # pip install mamba_ssm --upgrade
 #
-N_LAYER="12"
-N_EMBD="768"
+N_LAYER="8"
+N_EMBD="512"
 #
 CTX_LEN="1024" # !!! change magic_prime if you change ctx_len !!!
-PROJ_DIR="/pvc/RWKVout/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE"1024" # set output folder
+PROJ_DIR="/pvc/RWKVout/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE"8M" # set output folder
 #
 #######################################################################################################################
 #
@@ -27,7 +27,7 @@ PROJ_DIR="/pvc/RWKVout/L"$N_LAYER"-D"$N_EMBD"-"$MODEL_TYPE"1024" # set output fo
 # Larger model => use smaller LR
 # Finetuning => use very small LR, such as 1e-5
 #
-M_BSZ="32" # takes ~9G VRAM here => reduce this to save VRAM, increase this for faster speed
+M_BSZ="64" # takes ~9G VRAM here => reduce this to save VRAM, increase this for faster speed
 LR_INIT="6e-4"
 LR_FINAL="6e-5"
 GRAD_CP=0 # 1 => slower, save VRAM; 0 => faster, more VRAM
@@ -45,7 +45,7 @@ DS_BUCKET_MB=10 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affect
 #
 python train.py --load_model "0" --wandb "rna-rwkv" --proj_dir $PROJ_DIR --my_testing $MODEL_TYPE \
  --ctx_len $CTX_LEN --my_pile_stage 3 --epoch_count 999999 --epoch_begin 0 \
- --data_file "/pvc/dataset/train.npy" --my_exit_tokens 20480000000 --magic_prime 19999999 \
+ --data_file "/pvc/dataset/8M/8M.npy.npy" --my_exit_tokens 40960000000 --magic_prime 39999983 \
  --num_nodes $N_NODE --micro_bsz $M_BSZ --n_layer $N_LAYER --n_embd $N_EMBD --pre_ffn 0 --head_qk 0 \
  --lr_init $LR_INIT --lr_final $LR_FINAL --warmup_steps 50 --beta1 0.9 --beta2 0.99 --adam_eps 1e-18 --my_pile_edecay 0 --data_type "numpy" --vocab_size 9 \
  --weight_decay 0.01 --epoch_save $EPOCH_SAVE --head_size_a 64 \
