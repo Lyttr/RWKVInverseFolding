1a86335d1de9d67a42eda327557faafb46aa5ceb
Gavin Ji <71982068+Lyttr@users.noreply.github.com>
2025-08-19 03:23:48 -0700

    Update rna_generate.py

--- rna_generate.py
+++ rna_generate.py
@@ -102,7 +102,7 @@ def main():
     parser.add_argument("--chunk_size", type=int, default=10000)
     parser.add_argument("--min_len", type=int, default=12)
     parser.add_argument("--max_len", type=int, default=400)
-    parser.add_argument("--num_threads", type=int, default=16)
+    parser.add_argument("--num_threads", type=int, default=8)
     parser.add_argument("--rnafold_cmd", type=str, default="RNAfold")
     args = parser.parse_args()
 
